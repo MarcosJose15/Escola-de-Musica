16784709e88f6bc2afa0aa419168134d46585ce9
Marcos <marcosprofissional15@gmail.com>
2024-10-07 17:09:39 -0300

    add conexões

--- server.js
+++ server.js
@@ -1,16 +1,57 @@
-const express = require('express');
-const mysql = require('mysql');
+const express = require("express");
+const mysql = require('mysql2');
 const app = express();
-const port = 3000;
+const  { engine }  = require("express-handlebars");
+const bodyParser = require("body-parser");
+const port = 8083;
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'sua_senha',
-  database: 'nome_do_banco'
+//Config
+//Template Engine
+app.engine("handlebars", engine({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Conexão Banco
+const Sequelize = require("sequelize")
+const sequelize = new Sequelize ("EscolaMusica", "root", "teste", {
+    host: "localhost",
+    dialect: "mysql"
 });
 
-connection.connect(err => {
+sequelize.authenticate( ).then(function(){
+    console.log("Conectado com sucesso!")
+}).catch(function(erro){
+    console.log("Falha ao se conectar: "+erro);
+});
+
+const connection = mysql.createConnection({
+  host: 'localhost',          /* //127.0.0.1/3306 */
+  user: 'root',
+  password: 'teste',
+  database: 'EscolaMusica'
+});
+
+app.get("/site", function(req, res){
+  res.sendFile(__dirname + "/html/index.html");
+});
+
+// Rota para visualizar os dados
+app.get('/dados', (req, res) => {
+  connection.query('SELECT * FROM EscolaMusica', (error, results) => {
+    if (error) {
+      return res.status(500).send(error);
+    }
+    res.json(results);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
+/*connection.connect(err => {
   if (err) {
     console.error('Erro ao conectar: ' + err.stack);
     return;
@@ -25,6 +66,6 @@ app.get('/', (req, res) => {
   });
 });
 
-/*app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 }*/
